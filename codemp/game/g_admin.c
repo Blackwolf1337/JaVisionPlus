acd5400df59741594f6e2bf54ee676e6d66d8d58
Blackwolf1337 <wolfblack@hotmail.de>
2017-06-08 17:05:04 +0200

    Preparing stuff
    + preparing admin files
    + CMakeList
    + .gitignore for Visual Studio folders

---
+++ codemp/game/g_admin.c
@@ -0,0 +1,15 @@
+// JaVision+ administration system
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Admin data is saved in plain custom format (vision data) in <fs_homepath>/<fs_game>/admins.vd
+// Possible key encryption (AES 256)
+// --- Functionality ---
+
+#include "g_local.h"
